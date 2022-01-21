542ce97a6a18ded108cde28afa34f75db390bd96
bobntu426 <B08508021@ntu.edu.tw>
2022-01-22 02:05:41 +0800

    init frontend

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css'
+import { useState, useEffect } from 'react'
+import { Button, message } from 'antd'
+
+import styled from 'styled-components'
+
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Outlet,
+} from 'react-router-dom'
+
+
+function App() {
+  return<h1>全國大專院校一般組桌球盃賽網</h1>
+}
+
+export default App
